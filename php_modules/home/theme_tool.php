b32a6730c25cef7b87840f712a9fa94dcabb9000
木灵鱼儿 <1321968423@qq.com>
2023-03-22 05:47:55 +0800

    调整faq展示

--- php_modules/home/theme_tool.php
+++ php_modules/home/theme_tool.php
@@ -1,11 +1,13 @@
 <div class="theme-tool">
   <div class="theme-tool-content">
+    <?php if ($this->user->hasLogin()): ?>
     <a class="theme-tool-item" href="https: //www.yuque.com/mulingyuer/ohl5ed" target="_blank" title="FAQ主题使用指南">
       <div class="theme-tool-item-img-wrap">
         <img class="theme-tool-item-img" src="<?php $this->options->themeUrl('/static/images/theme-tool/faq.png');?>" alt="FAQ主题使用指南">
       </div>
       <h3 class="theme-tool-item-title">FAQ主题使用指南</h3>
     </a>
+    <?php endif;?>
     <a class="theme-tool-item" href="<?php $this->options->feedUrl();?>" target="_blank" title="博客文章RSS订阅">
       <div class="theme-tool-item-img-wrap">
         <img class="theme-tool-item-img" src="<?php $this->options->themeUrl('/static/images/theme-tool/rss_article.png');?>" alt="博客文章RSS订阅">
