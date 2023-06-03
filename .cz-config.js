b54e77a922df0545e184f6d5ce2194114c14137c
木灵鱼儿 <50077795+mulingyuer@users.noreply.github.com>
2023-06-04 03:47:22 +0800

    :rocket: chore: 更新git commit 规范相关内容

--- .cz-config.js
+++ .cz-config.js
@@ -1,7 +1,7 @@
 /*
  * @Author: mulingyuer
  * @Date: 2023-06-04 00:58:00
- * @LastEditTime: 2023-06-04 01:35:21
+ * @LastEditTime: 2023-06-04 03:36:27
  * @LastEditors: mulingyuer
  * @Description: git cz 配置文件
  * @FilePath: \Typecho_Theme_JJ\.cz-config.js
@@ -10,37 +10,41 @@
 module.exports = {
 	// type 类型（定义之后，可通过上下键选择）
 	types: [
-		{ value: ":sparkles: feat", name: "✨ feat: 新功能" },
-		{ value: ":bug: fix", name: "🐛 fix: 修复bug" },
-		{ value: ":lipstick: style", name: "💄 style: 更新界面与样式文件" },
-		{ value: ":recycle: refactor", name: "♻️ refactor: 重构" },
-		{ value: ":zap: perf", name: "⚡️ perf: 性能优化" },
-		{ value: ":art: style", name: "🎨 style: 代码格式（不影响功能，例如空格、分号等格式修正）" },
-		{ value: ":boom: breaking change", name: "💥 breaking change: 破坏性变更" },
-		{ value: ":mag: search", name: "🔍 search: 改进搜索引擎优化" },
-		{ value: ":package: build", name: "📦️ build: 打包" },
-		{ value: ":pencil2: docs", name: "✏️ docs: 文档变更" },
-		{ value: ":fire: delete", name: "🔥 delete: 删除代码或文件" },
-		{ value: ":arrow_up: upgrade", name: "⬆️ upgrade: 升级依赖" },
-		{ value: ":arrow_down: downgrade", name: "⬇️ downgrade: 降级依赖" },
-		{ value: ":pushpin: chore", name: "📌 chore: 固定依赖在特定版本" },
-		{ value: ":rewind: revert", name: "⏪ revert: 回滚代码" },
-		{ value: ":twisted_rightwards_arrows: merge", name: "🔀 merge: 合并分支" },
-		{ value: ":rocket: chore", name: "🚀 chore: 部署功能或性能优化(构建/工程依赖/工具)" },
-		{ value: ":tada: init", name: "🎉 init: 项目初始化" },
-		{ value: ":white_check_mark: test", name: "✅ test: 测试" },
-		{ value: " :construction: wip", name: "🚧 wip: 进行中的工作(自定义)" }
+		{ value: ":sparkles: feat", name: "✨  feat: 新功能" },
+		{ value: ":bug: fix", name: "🐛  fix: 修复bug" },
+		{ value: ":lipstick: style", name: "💄  style: 更新界面与样式文件" },
+		{ value: ":recycle: refactor", name: "♻️  refactor: 重构" },
+		{ value: ":zap: perf", name: "⚡  perf: 性能优化" },
+		{ value: ":art: style", name: "🎨  style: 代码格式（不影响功能，例如空格、分号等格式修正）" },
+		{ value: ":boom: breaking change", name: "💥  breaking change: 破坏性变更" },
+		{ value: ":mag: search", name: "🔍  search: 改进搜索引擎优化" },
+		{ value: ":package: build", name: "📦️  build: 打包" },
+		{ value: ":pencil2: docs", name: "✏️  docs: 文档变更" },
+		{ value: ":fire: delete", name: "🔥  delete: 删除代码或文件" },
+		{ value: ":arrow_up: upgrade", name: "⬆️  upgrade: 升级依赖" },
+		{ value: ":arrow_down: downgrade", name: "⬇️  downgrade: 降级依赖" },
+		{ value: ":pushpin: chore", name: "📌  chore: 固定依赖在特定版本" },
+		{ value: ":rewind: revert", name: "⏪  revert: 回滚代码" },
+		{ value: ":twisted_rightwards_arrows: merge", name: "🔀  merge: 合并分支" },
+		{ value: ":rocket: chore", name: "🚀  chore: 部署功能或性能优化(构建/工程依赖/工具)" },
+		{ value: ":tada: init", name: "🎉  init: 项目初始化" },
+		{ value: ":white_check_mark: test", name: "✅  test: 测试" },
+		{ value: " :construction: wip", name: "🚧  wip: 进行中的工作(自定义)" }
 	],
 
 	// scope 类型（定义之后，可通过上下键选择）
 	scopes: [
-		["components", "组件相关"],
-		["hooks", "hook 相关"],
+		["page", "页面相关"],
+		["component", "组件相关"],
 		["utils", "utils 相关"],
-		["element-ui", "对 element-ui 的调整"],
 		["styles", "样式相关"],
-		["deps", "项目依赖"],
-		["auth", "对 auth 修改"],
+		["API", "与 API 接口相关"],
+		["deps", "项目依赖相关"],
+		["config", "项目配置相关"],
+		["test", "测试相关"],
+		["build", "构建相关"],
+		["docs", "文档相关"],
+		["code arrange", "代码格式或整理相关"],
 		["other", "其他修改"],
 		// 如果选择 custom，后面会让你再输入一个自定义的 scope。也可以不设置此项，把后面的 allowCustomScopes 设置为 true
 		["custom", "以上都不是？我要自定义"]
@@ -74,7 +78,7 @@ module.exports = {
 	// 交互提示信息
 	messages: {
 		type: "确保本次提交遵循：前端代码规范！\n选择你要提交的类型：",
-		scope: "\n选择一个 scope（可选）：",
+		scope: "\n选择本次提交代码涉及的范围（可选）：",
 		// 选择 scope: custom 时会出下面的提示
 		customScope: "请输入自定义的 scope：",
 		subject: "填写简短精炼的变更描述：\n",
--- commitlint.config.js
+++ commitlint.config.js
@@ -1,7 +1,7 @@
 /*
  * @Author: mulingyuer
  * @Date: 2023-06-04 01:47:40
- * @LastEditTime: 2023-06-04 01:47:40
+ * @LastEditTime: 2023-06-04 03:43:01
  * @LastEditors: mulingyuer
  * @Description: commitlint 配置文件
  * @FilePath: \Typecho_Theme_JJ\commitlint.config.js
