0e421eed25200b5967d1710ee4747c7c9e1b6950
mulingyuer <1321968423@qq.com>
2023-07-09 17:25:40 +0800

    :lipstick: style: 调整日志输出

--- src/utils/ascii.ts
+++ src/utils/ascii.ts
@@ -1,59 +1,61 @@
 /*
  * @Author: mulingyuer
  * @Date: 2023-03-26 08:04:13
- * @LastEditTime: 2023-03-29 23:14:34
+ * @LastEditTime: 2023-07-09 17:24:08
  * @LastEditors: mulingyuer
  * @Description: ASCII 表情
- * @FilePath: \Typecho_Theme_JJ\src\utils\ascii.ts
+ * @FilePath: /Typecho_Theme_JJ/src/utils/ascii.ts
  * 怎么可能会有bug！！！
  */
 
 export default function asciiEmoji() {
-	console.log(
-		`%c
-                            voonna  uuuu!!                                          
-                            ~onnn     8auii                           -  *    *^    
-                           i1uaz%   i&$i81i8                               ^   *    
-             a~            ii!!!aa*******^^zz-              uz888uau                
-            .  &a    $$  %za111an*********;^~~o^aauaz31$$  81~~~~~~~~63             
-    *1*^~~~~~ova1u%%ii8;o*aauuun*************;;o**a~o;nn6!1$~~~~~~~~~~~^~v1in  *    
-#~~~~~~~~~~~~nao$6iin*****zaaaa;;;;;;;;;;;;*****~**a*****;oi%$$uua!66333663i11uu%   
-ou&11ii!!!iiii$6i%n~*****;;aaai;;;;;;;;;;;;;;;;;;;;;;;;;;;;;nii6%%!!i33!1aaaaaa1uaaa
-uaaaaaaa11i%6ii$z~***;;;;;;aaaz;;;;;;;;;;;;;;;;;;;;;~;;;;;;;;;uiii6633uai1iiaaaaaaaz
-a1311uua13!#%%!-;;;;;;;;;;;;aaa~~~~~;;;;;;;;;;;;;va;;;;;;;;;;;;~3%%$#3!36a1uuaaaaaaa
-iuaaaa!!!%%%%--;;;;;;;;;;;~n~aaa~~~~;;~;~;~uvz~;;111z;;~;~~~~~~~~#%%@   3i!iuuaaaz;3
-aaaa6!!   @%^-;;;;;;;;;;;vvv;z3aa~;~~~~~~~uuuuuuu1111a;~~~~~~~~~~~$8@#     $1uuuzzzz
-aaii!    3%3-;+;+;;;;;;;vvv-;zuzzaa;;;;zaaauuuuui1i11iz;~~~~~~~~~~$88@        8aazzz
-a1$      @8+^;;-;;-;;;;novv+vvzz*zzzozzzzzaaaaaa~311u31;au~~~~~~~~~86@           zzz
-&       .n+~un;;;;-;;-n;-vv*oaoov^v^vvvvvvvvzzu+.~uuau11a11a~~u1~~~o6$a            u
-       zza11i;;;1~1ooo;vvvznn*~oi+nnnnnnnoonnn*...~azau11111111111ou83%             
-    !vvzaauu1u;ii11no;vzv~~oo^.~o6ooooo~zo~~~*.....;vz;u1u111111111116              
-   ^3vzzaaui11iiiiunnazz~~~66...^on~~~~~na~~^^......ov;iuauuu1111a1113              
-         13uuiiiii1vnvvo~~......+.~~o~;;~^!.v.......!.+;avauuuuuuaaa1i          **  
-           11i1ii1uvvv6~~.....*-...u;;o;;....8;vn......~6vzazauuaaaaa1              
-           u111a11uivv8@@@@@#&+................-&#@@@i.;;vvzzauiiaaaa1         *    
-           1auaauazzv$~~.....^@@@...........3@@!.......+;vvzzaai3aaaa1              
-            auaazzzzvo~...@@@*..................@@@o....;ivzzaiu!iaaa8              
-            3uaaazzzz~*$@@..........................@@%.;8vzz1aa! aa1               
-             3uaaazaan^^^^^......................^^^^^^^;az31aaai a1                
-               uaazz13*^^^^.......uzvvvvv1+......^^^^^^;avuuaaai  8                 
-                -iaai1;...........ooonooooz.........---z3uzaaa.                     
-                 $iii11^..........oooooooo1...........!zzzaa8                       
-                 &81az11^.........ooooooon...........6uzza!                         
-                 & 1azz1i ^........zooooi..........8a11za                           
-                   &$az ^8   .u.............-;8     ^1iu                            
-                    ua  3@@@%%%6  ovvzoooo1vza~;;1                                  
-                    3           6;;n!3io81o--on..*.n                                
-                               ~...!o-nnn1oono+!..3v8$                              
-                             z$%u1i---3!!%~----~oi3&&%%$@                           
-                            u$$$%&~---nnniv++--on6&&&&$&&$&$&&                      
-                        $$$&$$$%&~o---~nnnzv++-~~~ &&&$%%$&&&&n                     
-                       v$$$$$$&&&1o---%$$nnnz++--*;o&&&&$$&&&&~o~.                  
-                      ~n$$$$$$&&&aoo--%66nnnno+--un~-%&&&%$$&&1oo.3                 
-                     ^;nz$$$$$&&&zo---nnnnnnnn-------&#&&&&$&i3o.3                  
-                     6;8i1$$$&&&~o~;^--%nnn%n-------&&&##!ii!                       
-                         1#&#&#&n------%$%%$------%$$&&&#1                          
-`,
-		"color:#71D1DB"
-	);
+	// 	console.log(
+	// 		`%c
+	//                             voonna  uuuu!!
+	//                             ~onnn     8auii                           -  *    *^
+	//                            i1uaz%   i&$i81i8                               ^   *
+	//              a~            ii!!!aa*******^^zz-              uz888uau
+	//             .  &a    $$  %za111an*********;^~~o^aauaz31$$  81~~~~~~~~63
+	//     *1*^~~~~~ova1u%%ii8;o*aauuun*************;;o**a~o;nn6!1$~~~~~~~~~~~^~v1in  *
+	// #~~~~~~~~~~~~nao$6iin*****zaaaa;;;;;;;;;;;;*****~**a*****;oi%$$uua!66333663i11uu%
+	// ou&11ii!!!iiii$6i%n~*****;;aaai;;;;;;;;;;;;;;;;;;;;;;;;;;;;;nii6%%!!i33!1aaaaaa1uaaa
+	// uaaaaaaa11i%6ii$z~***;;;;;;aaaz;;;;;;;;;;;;;;;;;;;;;~;;;;;;;;;uiii6633uai1iiaaaaaaaz
+	// a1311uua13!#%%!-;;;;;;;;;;;;aaa~~~~~;;;;;;;;;;;;;va;;;;;;;;;;;;~3%%$#3!36a1uuaaaaaaa
+	// iuaaaa!!!%%%%--;;;;;;;;;;;~n~aaa~~~~;;~;~;~uvz~;;111z;;~;~~~~~~~~#%%@   3i!iuuaaaz;3
+	// aaaa6!!   @%^-;;;;;;;;;;;vvv;z3aa~;~~~~~~~uuuuuuu1111a;~~~~~~~~~~~$8@#     $1uuuzzzz
+	// aaii!    3%3-;+;+;;;;;;;vvv-;zuzzaa;;;;zaaauuuuui1i11iz;~~~~~~~~~~$88@        8aazzz
+	// a1$      @8+^;;-;;-;;;;novv+vvzz*zzzozzzzzaaaaaa~311u31;au~~~~~~~~~86@           zzz
+	// &       .n+~un;;;;-;;-n;-vv*oaoov^v^vvvvvvvvzzu+.~uuau11a11a~~u1~~~o6$a            u
+	//        zza11i;;;1~1ooo;vvvznn*~oi+nnnnnnnoonnn*...~azau11111111111ou83%
+	//     !vvzaauu1u;ii11no;vzv~~oo^.~o6ooooo~zo~~~*.....;vz;u1u111111111116
+	//    ^3vzzaaui11iiiiunnazz~~~66...^on~~~~~na~~^^......ov;iuauuu1111a1113
+	//          13uuiiiii1vnvvo~~......+.~~o~;;~^!.v.......!.+;avauuuuuuaaa1i          **
+	//            11i1ii1uvvv6~~.....*-...u;;o;;....8;vn......~6vzazauuaaaaa1
+	//            u111a11uivv8@@@@@#&+................-&#@@@i.;;vvzzauiiaaaa1         *
+	//            1auaauazzv$~~.....^@@@...........3@@!.......+;vvzzaai3aaaa1
+	//             auaazzzzvo~...@@@*..................@@@o....;ivzzaiu!iaaa8
+	//             3uaaazzzz~*$@@..........................@@%.;8vzz1aa! aa1
+	//              3uaaazaan^^^^^......................^^^^^^^;az31aaai a1
+	//                uaazz13*^^^^.......uzvvvvv1+......^^^^^^;avuuaaai  8
+	//                 -iaai1;...........ooonooooz.........---z3uzaaa.
+	//                  $iii11^..........oooooooo1...........!zzzaa8
+	//                  &81az11^.........ooooooon...........6uzza!
+	//                  & 1azz1i ^........zooooi..........8a11za
+	//                    &$az ^8   .u.............-;8     ^1iu
+	//                     ua  3@@@%%%6  ovvzoooo1vza~;;1
+	//                     3           6;;n!3io81o--on..*.n
+	//                                ~...!o-nnn1oono+!..3v8$
+	//                              z$%u1i---3!!%~----~oi3&&%%$@
+	//                             u$$$%&~---nnniv++--on6&&&&$&&$&$&&
+	//                         $$$&$$$%&~o---~nnnzv++-~~~ &&&$%%$&&&&n
+	//                        v$$$$$$&&&1o---%$$nnnz++--*;o&&&&$$&&&&~o~.
+	//                       ~n$$$$$$&&&aoo--%66nnnno+--un~-%&&&%$$&&1oo.3
+	//                      ^;nz$$$$$&&&zo---nnnnnnnn-------&#&&&&$&i3o.3
+	//                      6;8i1$$$&&&~o~;^--%nnn%n-------&&&##!ii!
+	//                          1#&#&#&n------%$%%$------%$$&&&#1
+	// `,
+	// 		"color:#71D1DB"
+	// 	);
+
+	console.log("%cWelcome to Blog!", "color: #1E80FF; font-size: x-large;padding: 10px;");
 }
